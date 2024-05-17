f2c4dafb0e7442730ac4780e68de04cda0f9f4f9
William Roos <23wiro@ad.ssis.nu>
2024-05-17 21:45:46 +0200

    piss

--- test.c
+++ test.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <wchar.h>
 
-
-int board[8][8];
+char board[8][8];
 int i, j, k;
-
 
 struct Node {
     int snakeX;
@@ -13,35 +12,35 @@ struct Node {
     struct Node* next;
 };
 
-struct Node* addNode(int x, int y){
+struct Node* addNode(struct Node* head, int x, int y) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     if (newNode == NULL) {
-        printf("Memory not allocated");
-        return 0;
+        printf("Memory not allocated\n");
+        return head;
     }
     newNode->snakeX = x;
     newNode->snakeY = y;
-    newNode->next = NULL;
+    newNode->next = head;
     return newNode;
 }
 
-int getDataOfIndex(struct Node* head,int index, int* x, int* y) {
+int getDataOfIndex(struct Node* head, int index, int* x, int* y) {
     if (head == NULL) {
         printf("List is empty\n");
-        exit(1);
+        return 0;
     }
 
     struct Node* current = head;
     int currentIndex = 0;
-    while (current!= NULL && currentIndex < index) {
-    {
+
+    while (current != NULL && currentIndex < index) {
         current = current->next;
         currentIndex++;
     }
 
-    if (current!= NULL) {
+    if (current == NULL) {
         printf("Index out of bounds\n");
-        exit(1);
+        return 0;
     }
 
     *x = current->snakeX;
@@ -49,26 +48,43 @@ int getDataOfIndex(struct Node* head,int index, int* x, int* y) {
     return 1;
 }
 
-int main()  {
+int main() {
     struct Node* head = NULL;
-    addNode(0, 0);
-    addNode(0, 1);
-    addNode(0, 2);
+    head = addNode(head, 0, 2);
+    head = addNode(head, 0, 1);
+    head = addNode(head, 0, 0);
 
     srand(time(NULL));
-    int appleX = rand() %8;
-    int appleY = rand() %8;
-    board[appleX][appleY] = 1;
+    int appleX = rand() % 8;
+    int appleY = rand() % 8;
 
     int snakeX, snakeY;
 
-    for (i=0; i <= 7; i++) {
-        for (j=0; j<=7; j++){
-            for (k=0; getDataOfIndex(head, k, &snakeX, &snakeY) != NULL; k++){
-                board[snakeX][snakeY] = 2;
-            }
-            printf("%d ", board[i][j]);
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            board[i][j] = 0; 
+        }
+    }
+    board[appleX][appleY] = "X";
+
+    for (k = 0; getDataOfIndex(head, k, &snakeX, &snakeY); k++) {
+        board[snakeX][snakeY] = 2;  // Place the snake on the board
+    }
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            printf("%s ", board[i][j]);
         }
         printf("\n");
     }
+
+    // Free the allocated memory
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
 }
