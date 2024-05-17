9c824151c7958eeef52c91db31d2159771bf39b6
23wiro <william.1.roos@gmail.com>
2024-05-17 23:37:21 +0200

    bollar

--- test.c
+++ test.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <wchar.h>
 
 char board[8][8];
 int i, j, k;
@@ -24,7 +23,7 @@ struct Node* addNode(struct Node* head, int x, int y) {
     return newNode;
 }
 
-int getDataOfIndex(struct Node* head, int index, int* x, int* y) {
+int getDataOfIndex(struct Node *head, int index, int* x, int* y) {
     if (head == NULL) {
         printf("List is empty\n");
         return 0;
@@ -48,6 +47,34 @@ int getDataOfIndex(struct Node* head, int index, int* x, int* y) {
     return 1;
 }
 
+int setDataOfIndex(struct Node *head, int index, int x, int y) {
+    struct Node* current = head;
+    int currentIndex = 0;
+
+    while (current != NULL && currentIndex < index) {
+        current = current->next;
+        currentIndex++;
+    }
+    if (current == NULL) {
+        printf("Index out of bounds\n");
+        return 0;
+    }
+    current->snakeX = x;
+    current->snakeY = y;
+}
+
+int chuckThatShitOutOfHere(int *getDataOfIndex,  int *setDataOfIndex, Node *head) {
+    struct Node* current = head;
+    int l = 0;
+    while (current != NULL){
+        current = current->next;
+        l++;
+
+    }
+    getDataOfIndex(l, &snakeX, &snakeY);
+    setDataOfIndex();
+}
+
 int main() {
     struct Node* head = NULL;
     head = addNode(head, 0, 2);
@@ -65,15 +92,19 @@ int main() {
             board[i][j] = 0; 
         }
     }
-    board[appleX][appleY] = "X";
+    board[appleX][appleY] = 1;
 
-    for (k = 0; getDataOfIndex(head, k, &snakeX, &snakeY); k++) {
+    struct Node* current = head;
+    while (current != NULL) {
+        snakeX = current->snakeX;
+        snakeY = current->snakeY;
         board[snakeX][snakeY] = 2;  // Place the snake on the board
+        current = current->next;
     }
 
     for (i = 0; i < 8; i++) {
         for (j = 0; j < 8; j++) {
-            printf("%s ", board[i][j]);
+            printf("%d ", board[i][j]);  // Use %d instead of %s
         }
         printf("\n");
     }
